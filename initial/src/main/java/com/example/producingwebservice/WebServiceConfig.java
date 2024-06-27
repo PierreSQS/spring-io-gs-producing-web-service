3b08222613138f50988bb00db63526230e63a8b8
PierreSQS <nomo_messina@web.de>
2024-06-27 15:44:07 +0200

    configured the Web Service Bean

---
+++ initial/src/main/java/com/example/producingwebservice/WebServiceConfig.java
@@ -0,0 +1,42 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet>
+    messageDispatcherServletServletRegistration(ApplicationContext appCxt) {
+
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(appCxt);
+        messageDispatcherServlet.setTransformWsdlLocations(true);
+
+        return new ServletRegistrationBean<>(messageDispatcherServlet,"/ws/**");
+
+    }
+
+
+    @Bean
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema xsdSchema) {
+        DefaultWsdl11Definition defaultWsdl11Definition = new DefaultWsdl11Definition();
+        defaultWsdl11Definition.setPortTypeName("CountriesPort");
+        defaultWsdl11Definition.setLocationUri("/ws");
+        defaultWsdl11Definition.setLocationUri("http://spring.io/guides/gs-producing-web-service");
+        defaultWsdl11Definition.setSchema(xsdSchema);
+        return defaultWsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/countries.xsd"));
+    }
+}
